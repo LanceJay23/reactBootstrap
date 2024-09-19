a9010cb1709a83c0af00c98242ac5c126c21d65e
LanceJay023 <lancejayslvalenzuela2gmail.com>
2024-09-19 09:04:00 +0800

    index

--- src/mainpage/index.js
+++ src/mainpage/index.js
@@ -1,13 +1,13 @@
 import Navbar from "./Navbar";
 import Contain from "./Contain";
-import Account from "./account";
+import Account from "./Account";
 import Boxes from "./Boxes";
 import Fundament from "./Fundament";
 import Reacts from "./Reacts";
 import Questions from "./Questions";
 import Instructors from "./Instructors";
 import Contacts from "./Contacts";
-import Footer from "./footer";
-import Mmodal from "./mmodal";
+import Footer from "./Footer";
+import Mmodal from "./Mmodal";
 
 export {Navbar, Contain, Account, Boxes, Fundament, Reacts, Questions, Instructors, Contacts, Footer, Mmodal}
